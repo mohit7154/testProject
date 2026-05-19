ff5e3b633df1baefcf02d0007d8a506960fe6f3b
mohit7154 <53005878+mohit7154@users.noreply.github.com>
2026-05-20 02:27:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+var currentColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+const button = document.getElementById("myButton");
+
+button.onmouseover = function() {
+  button.style.backgroundColor = currentColor;
+};
+
+button.onmouseout = function() {
+  button.style.backgroundColor = "white";
+};
+
 function changeText() {
-  var message = document.getElementById("message");
-  switch(message.innerText){
-    case "This is Day 4 of my consistency streak.":
-      message.innerText = "Button clicked. I'm still showing up.";
-      break;
-    default:
-      message.innerText = "Button clicked. I'm still showing up.";
-  }
-  message.style.color = "#" + Math.floor(Math.random()*16777215).toString(16);
+  const message = document.getElementById("message");
+  message.innerText = "Button clicked. I'm still showing up.";
+  message.style.color = currentColor;
+  currentColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
