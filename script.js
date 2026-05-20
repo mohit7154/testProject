e0c1dde13bce61177e59688a1b50e2cbdec43b12
mohit7154 <53005878+mohit7154@users.noreply.github.com>
2026-05-20 17:25:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var currentColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
 const button = document.getElementById("myButton");
-
+const name = document.getElementById("nameInput");
+name.style.backgroundColor = currentColor;
 
 button.onmouseover = function() {
   button.style.backgroundColor = currentColor;
@@ -12,11 +13,10 @@ button.onmouseout = function() {
 
 function changeText() {
   const message = document.getElementById("message");
-  const name = document.getElementById("nameInput").value + '';
-  if(name.trim() == "")
-    name = "BlahBlah";
-  message.innerText = "Button clicked. "+ name +", you're still showing up.";
+  if((name.value+'').trim() == "")
+    name.value = "BlahBlah";
+  message.innerText = "Button clicked. "+ name.value +", you're still showing up.";
   message.style.color = currentColor;
   currentColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  document.getElementById("nameInput").value = ""
+  name.value = ""
 }
