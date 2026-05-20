6bd3714d7aa018b8c49057050ad4718d85ba0a71
mohit7154 <53005878+mohit7154@users.noreply.github.com>
2026-05-20 17:19:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,10 +12,11 @@ button.onmouseout = function() {
 
 function changeText() {
   const message = document.getElementById("message");
-  const name = document.getElementById("nameInput").value;
-  if(name === "")
+  const name = document.getElementById("nameInput").value + '';
+  if(name.trim() == "")
     name = "BlahBlah";
   message.innerText = "Button clicked. "+ name +", you're still showing up.";
   message.style.color = currentColor;
   currentColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  document.getElementById("nameInput").value = ""
 }
