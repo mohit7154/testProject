2b377b0eb5c085c826bc4ee43da41c669227981c
mohit7154 <53005878+mohit7154@users.noreply.github.com>
2026-05-21 21:00:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,54 @@ const button = document.getElementById("myButton");
 const name = document.getElementById("nameInput");
 
 
-button.onmouseover = function() {
+button.onmouseover = function () {
   button.style.backgroundColor = currentColor;
   name.style.backgroundColor = currentColor;
   name.style.transform = "scale(2)";
 };
 
-button.onmouseout = function() {
+button.onmouseout = function () {
   button.style.backgroundColor = null;
   name.style.backgroundColor = null;
   name.style.transform = null;
 };
+const tasks = [];
 
+function addTask() {
+
+  const taskInput = document.getElementById("taskInput");
+  const task = taskInput.value.trim();
+
+  if (task === "") {
+    return;
+  }
+
+  tasks.push(task);
+  renderTasks();
+  taskInput.value = "";
+}
 function changeText() {
   const message = document.getElementById("message");
-  if((name.value+'').trim() == "")
+  if ((name.value + '').trim() == "")
     name.value = "BlahBlah";
-  message.innerText = "Button clicked. "+ name.value +", you're still showing up.";
+  message.innerText = "Button clicked. " + name.value + ", you're still showing up.";
   message.style.color = currentColor;
   currentColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
   button.style.backgroundColor = currentColor;
   name.style.backgroundColor = currentColor;
   name.value = "";
+}
+
+function renderTasks() { 
+
+  const taskList =
+    document.getElementById("taskList");
+
+  taskList.innerHTML = "";
+
+  for (let i = 0; i < tasks.length; i++) {
+
+    taskList.innerHTML +=
+      `<li>${tasks[i]}</li>`;
+  }
 }
